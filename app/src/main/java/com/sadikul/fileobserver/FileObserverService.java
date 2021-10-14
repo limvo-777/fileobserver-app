03e07b5215830e8ae40882ce0a9ffa06892feda6
limvo-777 <lejpf54@gmail.com>
2021-10-14 19:59:09 +0900

    version 2

--- app/src/main/java/com/sadikul/fileobserver/FileObserverService.java
+++ app/src/main/java/com/sadikul/fileobserver/FileObserverService.java
@@ -1,176 +1,41 @@
-package com.sadikul.fileobserver;
-
-import android.app.ActivityManager;
-import android.app.Service;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Environment;
-import android.os.FileObserver;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
-import android.text.TextUtils;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class FileObserverService extends Service
-{
-
-
-    FileObserver mFileObserver = null;
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d("obService","started");
-
-        return super.onStartCommand(intent, flags, startId);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-
-        Log.d("obService","onCreate");
-        String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-        Log.d("obService yhs1",path);
-
-
-
-        mFileObserver = new FileObserver(path) {
-            @Override
-            public void onEvent(int event, @Nullable String path) {
-                ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-                List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfoList = am.getRunningAppProcesses();
-                List<String> processNameList = new ArrayList<String>();
-
-                for (ActivityManager.RunningAppProcessInfo rapi : runningAppProcessInfoList) {
-                    processNameList.add(rapi.processName);}
-                String service1 = TextUtils.join(", ", processNameList);
-                String service = "";
-                Log.d("obService","yhs2:" + path + ":" + event);
-
-                int evt = event & FileObserver.ALL_EVENTS;
-
-                if(evt == FileObserver.ACCESS) {
-                    Log.d("obService","yhs3: ACCESS");
-                    if(path != null) {
-                        sendMessage("ACCESS:" + path + service);
-                    }
-                }
-
-                if (evt == FileObserver.OPEN) {
-                    Log.d("obService","yhs4: OPEN");
-                    Log.d("serviceinfo","SERVICE : "+service1);
-                    if(path != null) {
-                        sendMessage("OPEN:" + path + service);
-                    }
-                }
-
-                if (evt == FileObserver.CREATE) {
-                    Log.d("obService","yhs3: CREATE");
-
-                    if(path != null) {
-                        sendMessage("CREATE:" + path + service);
-                    }
-
-                }
-
-                if (evt == FileObserver.ATTRIB) {
-                    Log.d("obService","yhs3: ATTRIB");
-                    if(path != null) {
-                        sendMessage("ATTRIB:" + path + service);
-                    }
-
-                }
-
-                if (evt == FileObserver.CREATE) {
-                    Log.d("obService","yhs3: CREATE");
-                    if(path != null) {
-                        sendMessage("CREATE:" + path + service);
-                    }
-
-                }
-
-                if (evt == FileObserver.CLOSE_NOWRITE) {
-                    Log.d("obService","yhs3: CLOSE_NOWRITE");
-                    if(path != null) {
-                        sendMessage("CLOSE_NOWRITE:" + path + service);
-                    }
-                }
-
-                if (evt == FileObserver.CLOSE_WRITE) {
-                    Log.d("obService","yhs3: CLOSE_WRITE");
-                    if(path != null) {
-                        sendMessage("CLOSE_WRITE:" + path + service);
-                    }
-
-                }
-                if (evt == FileObserver.DELETE) {
-                    Log.d("obService","yhs3: DELETE");
-                    if(path != null) {
-                        sendMessage("DELETE:" + path + service);
-                    }
-                }
-                if (evt == FileObserver.DELETE_SELF) {
-                    Log.d("obService","yhs3: DELETE_SELF");
-                    if(path != null) {
-                        sendMessage("DELETE_SELF:" + path + service);
-                    }
-                }
-                if (evt == FileObserver.MODIFY) {
-                    Log.d("obService","yhs3: MODIFY");
-                    if(path != null) {
-                        sendMessage("MODIFY:" + path + service);
-                    }
-                }
-                if (evt == FileObserver.MOVE_SELF) {
-                    Log.d("obService","yhs3: MOVE_SELF");
-                    if(path != null) {
-                        sendMessage("MOVE_SELF:" + path + service);
-                    }
-                }
-                if (evt == FileObserver.MOVED_FROM) {
-                    Log.d("obService","yhs3: MOVED_FROM");
-                    if(path != null) {
-                        sendMessage("MOVED_FROM:" + path + service);
-                    }
-
-                }
-                if (evt == FileObserver.MOVED_TO) {
-                    Log.d("obService","yhs3: MOVED_TO");
-                    if(path != null) {
-                        sendMessage("MOVED_TO:" + path + service);
-                    }
-                }
-
-
-                // 서비스에서 엑티비티로 데이터 전달 https://onepinetwopine.tistory.com/283
-                //final TextView text = (TextView) getView().findViewById(R.id.textView1);
-                //text.setText(event);
-            }
-        };
-
-        mFileObserver.startWatching();
-    }
-
-    private void sendMessage(String msg) {
-        Intent intent = new Intent("yhsIntent");
-        intent.putExtra("alertStr",msg);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-    }
-
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-    }
-}
+//package com.sadikul.fileobserver;
+//
+//import android.app.ActivityManager;
+//import android.app.Service;
+//import android.content.Context;
+//import android.content.Intent;
+//import android.os.Environment;
+//import android.os.FileObserver;
+//import android.os.IBinder;
+//import android.support.annotation.Nullable;
+//import android.support.v4.content.LocalBroadcastManager;
+//import android.text.TextUtils;
+//import android.util.Log;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class FileObserverService extends Service
+//{
+//
+//
+//    FileObserver mFileObserver = null;
+//
+//    private void sendMessage(String msg) {
+//        Intent intent = new Intent("yhsIntent");
+//        intent.putExtra("alertStr",msg);
+//        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+//    }
+//
+//
+//    @Nullable
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onDestroy() {
+//        super.onDestroy();
+//    }
+//}
--- app/src/main/java/com/sadikul/fileobserver/MainActivity.java
+++ app/src/main/java/com/sadikul/fileobserver/MainActivity.java
@@ -1,48 +1,28 @@
-//package com.sadikul.fileobserver;
-//
-//import android.content.Intent;
-//import android.os.Environment;
-//import android.os.FileObserver;
-//import android.support.annotation.Nullable;
-//import android.support.v4.os.EnvironmentCompat;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.util.Log;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        startService(new Intent(this,FileObserverService.class));
-//    }
-//
-//
-//}
-
 package com.sadikul.fileobserver;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
+import android.app.ActivityManager;
+import android.app.AppOpsManager;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
 import android.os.FileObserver;
-import android.support.annotation.Nullable;
-import android.support.constraint.solver.widgets.Helper;
-import android.support.v4.content.LocalBroadcastManager;
-import android.support.v4.os.EnvironmentCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
+import android.util.LongSparseArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -50,13 +30,15 @@ import java.util.HashSet;
 import java.util.List;
 
 public class MainActivity extends Activity {
+    CheckPackageNameThread checkPackageNameThread;
 
     public static final List<String> list = new ArrayList<>();
     public static final List<String> listL = new ArrayList<>();
+    public static String procName = new String();
     public static final ArrayList<TestFileObserver> sListFileObserver = new ArrayList<TestFileObserver>();
 
 
-    static class TestFileObserver extends FileObserver {
+    class TestFileObserver extends FileObserver {
         private String mPath;
 
         int[] eventValue = new int[] {FileObserver.ACCESS, FileObserver.ALL_EVENTS, FileObserver.ATTRIB, FileObserver.CLOSE_NOWRITE,FileObserver.CLOSE_WRITE, FileObserver.CREATE,
@@ -67,35 +49,49 @@ public class MainActivity extends Activity {
 
         public TestFileObserver(String path) {
             super(path);
+            Log.i("XXpath", path);
             mPath = path;
             sListFileObserver.add(this);
         }
 
+
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
         public void onEvent(int event, String path) {
             StringBuilder strEvents = new StringBuilder();
-            strEvents.append("Event : ").append('(').append(event).append(')');
+//            strEvents.append("Event : ").append('(').append(event).append(')');
+            if (path==null)
+            {
+                path = mPath.substring(mPath.lastIndexOf("/")+1);
+            }
             for (int i = 0; i < eventValue.length; ++i) {
                 if ((eventValue[i] & event) == eventValue[i]) {
-                    strEvents.append(eventName[i]);
-                    strEvents.append(',');
+                    strEvents.append("Event : "+eventName[i]);
+                    strEvents.append('\n');
                 }
             }
-            if ((event & FileObserver.DELETE_SELF) == FileObserver.DELETE_SELF) {
-                sListFileObserver.remove(this);
-            }
-            strEvents.append("\tPath : ").append(path).append('(').append(mPath).append(')');
-            Log.i("FileObserver", strEvents.toString());
-            if (list.isEmpty()) {
-                list.add(strEvents.toString());
-            } else {
-                if (list.get(list.size() - 1) != strEvents.toString()) {
+            if (strEvents.toString().length()!=0 && procName!=null){
+                if ((event & FileObserver.DELETE_SELF) == FileObserver.DELETE_SELF) {
+                    sListFileObserver.remove(this);
+                }
+
+                strEvents.append("Name  : ").append(path+"\n").append("Path   : ").append(mPath+'\n');
+                serviceInfo2();
+                strEvents.append("Proc  : \n").append(procName);
+
+                Log.i("FileObserver", strEvents.toString());
+                if (list.isEmpty()) {
                     list.add(strEvents.toString());
+                } else {
+                    if (list.get(list.size() - 1) != strEvents.toString()) {
+                        list.add(strEvents.toString());
+                    }
+                }
+                if (list.size() > 1000) {
+                    list.clear();
                 }
             }
-            if (list.size() > 1000) {
-                list.clear();
-            }
+
 
         }
     }
@@ -105,45 +101,255 @@ public class MainActivity extends Activity {
 
             setContentView(R.layout.activity_main);
 
+            checkPackageNameThread = new CheckPackageNameThread();
+            checkPackageNameThread.start();
 
             final ListView listview = (ListView)findViewById(R.id.listviewA);
-            //리스트뷰의 어댑터를 지정해준다.
+            //리스트뷰의 어댑터 지정
             final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                     android.R.layout.simple_list_item_1, listL);
-            listview.setAdapter(adapter);
-            listL.add("==== LOG START ====");
-            //String path = Environment.getExternalStorageDirectory() + "/Test";
-            //File f = new File(path);
 
-            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            final EditText edittext=(EditText)findViewById(R.id.editTextPath);
+
+
+
+
+            //START 버튼
+            Button button=(Button)findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @RequiresApi(api = Build.VERSION_CODES.KITKAT)
                 @Override
-                public void onItemClick(AdapterView<?> a_parent, View a_view, int a_position, long a_id) {
+                public void onClick(View v) {
+                    if(!checkPermission())
+                        startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+
+                    //edittext 비활성화
+                    edittext.setClickable(false);
+                    edittext.setFocusable(false);
+
+                    listview.setAdapter(adapter);
+                    listL.add("==== LOG START ====");
+
+
+                    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> a_parent, View a_view, int a_position, long a_id) {
+
+                        }
+                    });
+                    String path = String.valueOf(edittext.getText());
+                    String mpath;
+                    if (path!=""){
+                        mpath = Environment.getExternalStorageDirectory() + path;
+                    }else{
+                        mpath = String.valueOf(Environment.getExternalStorageDirectory());
+                    }
+                    File f = new File(mpath);
+//                    monitorAllFiles(path);
+                    monitorAllFiles(f);
+                }
+            });
+
+            //STOP 버튼
+            Button button2=(Button)findViewById(R.id.button2);
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    edittext.setFocusableInTouchMode (true);
+                    edittext.setFocusable(true);
+                    for(TestFileObserver fob : sListFileObserver){
+                        fob.stopWatching();
+                    }
                     HashSet<String> hlist = new HashSet<>(list);
                     listL.addAll(hlist);
                     list.clear();
+                    listL.add("==== LOG STOP ====");
+                    listview.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+
+                }
+            });
+
+            //CLEAR 버튼
+            Button button3=(Button)findViewById(R.id.button3);
+            button3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listL.clear();
+                    listL.add("==== LOG CLEAR ====");
                     listview.setAdapter(adapter);
                     adapter.notifyDataSetChanged();
                 }
             });
-
-            monitorAllFiles();
-
+            //UPDATE 버튼
+            Button button4=(Button)findViewById(R.id.button4);
+            button4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (list.size()!=0){
+                        HashSet<String> hlist = new HashSet<>(list);
+                        listL.addAll(hlist);
+                        list.clear();
+                    }
+                    listview.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+            });
         }
 
 
+        private void monitorAllFiles(File root) {
+//        private void monitorAllFiles(String path) {
+        //하위경로 포함
+            try {
+                File[] files = root.listFiles();
+                if (files.length != 0) {
+                    for (File file : files) {
+                        TestFileObserver fileObserver = new TestFileObserver(file.getAbsolutePath());
+                        fileObserver.startWatching();
+                        if (file.isDirectory()) monitorAllFiles(file);
+                    }
+                }
+                throw new Exception();
 
-    private void monitorAllFiles() {
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
 
-   //     File[] files = root.listFiles();
-   //     for(File file : files) {
-   //         TestFileObserver fileObserver = new TestFileObserver(file.getAbsolutePath());
-   //         fileObserver.startWatching();
-                 TestFileObserver fileObserver = new TestFileObserver("/storage/emulated/0/Test");
-                 fileObserver.startWatching();
+//            해당 디렉터리 내 파일, 디렉터리만
+//            String Opath = Environment.getExternalStorageDirectory() + path;
+//            TestFileObserver fileObserver = new TestFileObserver(Opath);
+//            fileObserver.startWatching();
 
-   //         if(file.isDirectory()) monitorAllFiles(file);
-   //     }
+         }
+        public String serviceInfo(){
+            StringBuilder strProc = new StringBuilder();
+            ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
+            List<ActivityManager.RunningAppProcessInfo> runningAppProcessInfoList = am.getRunningAppProcesses();
+            strProc.append("procsize : "+runningAppProcessInfoList.size()+"\n");
+            List<String> processNameList = new ArrayList<String>();
+            for (ActivityManager.RunningAppProcessInfo procinfo : runningAppProcessInfoList) {
+                processNameList.add(procinfo.processName);
+                strProc.append(procinfo.processName+"\n");
+            }
+
+            return strProc.toString();
+        }
+
+        // 프로세스 정보 가져오기
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void serviceInfo2(){
+        if(!checkPermission())
+        {startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));}
+
     }
+
+    private class CheckPackageNameThread extends Thread{
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        public void run(){
+            while(true){
+                if(!checkPermission()) continue;
+//                System.out.println(getPackageName(getApplicationContext()));
+                Log.i("XXapp",getPackageName(getApplicationContext()));
+                procName=getPackageName(getApplicationContext());
+                try {
+
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private boolean checkPermission(){
+
+        boolean granted = false;
+
+        AppOpsManager appOps = (AppOpsManager) getApplicationContext()
+                .getSystemService(Context.APP_OPS_SERVICE);
+
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getApplicationContext().getPackageName());
+
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            granted = (getApplicationContext().checkCallingOrSelfPermission(
+                    android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+        }
+        else {
+            granted = (mode == AppOpsManager.MODE_ALLOWED);
+        }
+
+        return granted;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public static String getPackageName(@NonNull Context context) {
+
+        // UsageStatsManager 선언
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        long lastRunAppTimeStamp = 0L;
+        long previousRunAppTimeStamp=0L;
+
+        // 얼마만큼의 시간동안 수집한 앱의 이름을 가져오는지 정하기 (begin ~ end 까지의 앱 이름을 수집한다)
+        final long INTERVAL = 10000;
+        final long end = System.currentTimeMillis();
+        // 1 minute ago
+        final long begin = end - INTERVAL;
+        String lastProc ;
+        String preProc;
+
+        //
+        LongSparseArray packageNameMap = new LongSparseArray<>();
+
+        // 수집한 이벤트들을 담기 위한 UsageEvents
+        final UsageEvents usageEvents = usageStatsManager.queryEvents(begin, end);
+        // 이벤트가 여러개 있을 경우 (최소 존재는 해야 hasNextEvent가 null이 아니니까)
+        while (usageEvents.hasNextEvent()) {
+
+            // 현재 이벤트를 가져오기
+            UsageEvents.Event event = new UsageEvents.Event();
+            usageEvents.getNextEvent(event);
+            // 현재 이벤트가 포그라운드 상태라면 = 현재 화면에 보이는 앱이라면
+            if(isForeGroundEvent(event)) {
+                // 해당 앱 이름을 packageNameMap에 넣는다.
+                packageNameMap.put(event.getTimeStamp(), event.getPackageName());
+
+                // 가장 최근에 실행 된 이벤트에 대한 타임스탬프를 업데이트 해준다.
+                if(event.getTimeStamp() > lastRunAppTimeStamp) {
+                    previousRunAppTimeStamp = lastRunAppTimeStamp;
+                    lastRunAppTimeStamp = event.getTimeStamp();
+                }
+            }
+        }
+
+        lastProc = packageNameMap.get(lastRunAppTimeStamp, "").toString();
+        preProc =packageNameMap.get(previousRunAppTimeStamp, "").toString();
+        if (lastProc!=preProc)
+            lastProc+="\n"+preProc;
+
+        // 가장 마지막까지 있는 앱의 이름을 리턴해준다.
+        return lastProc;
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private static boolean isForeGroundEvent(UsageEvents.Event event) {
+
+        if(event == null) return false;
+
+        if(BuildConfig.VERSION_CODE >= 29){
+//            return event.getEventType() == UsageEvents.Event.ACTIVITY_RESUMED;
+        }
+
+
+        return event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND;
+    }
+
 }
 
 
